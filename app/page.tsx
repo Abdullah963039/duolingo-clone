051ac11aab3e2b1fee2669b43dc8bbcdf70efa8c
Abdullah963039 <abdullahalwaraa039@gmail.com>
2024-03-15 20:43:10 +0300

    cusomize buttons

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,26 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div>
-      <p className="text-green-500 text-6xl">Hello Lingo</p>
+    <div className="flex items-center justify-center flex-col gap-y-4 my-9">
+      <Button>DEFAULT</Button>
+
+      <Button variant="primary">PRIMARY</Button>
+      <Button variant="primary-outline">PRIMARY outline</Button>
+
+      <Button variant="secondary">seconday</Button>
+      <Button variant="secondary-outline">seconday outline</Button>
+
+      <Button variant="danger">danger</Button>
+      <Button variant="danger-outline">danger outline</Button>
+
+      <Button variant="super">super</Button>
+      <Button variant="super-outline">super outline</Button>
+
+      <Button variant="sidebar">sidebar</Button>
+      <Button variant="sidebar-outline">sidebar outline</Button>
+
+      <Button variant="ghost">ghost</Button>
     </div>
   );
 }
